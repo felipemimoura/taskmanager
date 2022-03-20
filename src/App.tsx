7d30f9e9c7893d9ec2dd6a18106eaa51c3d408b4
Felipe Moura <felipe.moura@interprocess.com.br>
2022-03-20 19:31:53 -0300

    feat: add TaskProvider at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { TaskProvider } from './contexts/task'
 import Router from './routes'
 import { GlobalStyles } from './styles/global'
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
-      <GlobalStyles />
+      <TaskProvider>
+        <Router />
+        <GlobalStyles />
+      </TaskProvider>
     </BrowserRouter>
   )
 }
